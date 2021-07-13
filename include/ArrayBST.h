24e11b988e8811aa0d71868e8458fa92bc8d8842
Nirbhay Adhikari <fearalert1@gmail.com>
2021-07-13 11:19:56 +0545

    ArrayBST.h created

---
+++ include/ArrayBST.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define MAX_NUM_NODES 128
+
+#include "AbstractBST.h"
+
+class ArrayNode{
+    public:
+    int key;
+    int value;
+
+    ArrayNode(int k, int val){
+        key=k;
+        value=val;
+    }
+
+};
+
+class ArrayBST: public AbstractBST{
+
+    public:
+    ArrayBST();
+    ~ArrayBST();
+
+    bool isEmpty();
+    bool add(int key, int value);
+    void max(int &output);
+    void min(int &output);
+    bool exists(int targetkey);
+
+    void inorder();
+
+    private:
+    ArrayNode *nodes[MAX_NUM_NODES];
+    void inorder(int index);
+
+};
