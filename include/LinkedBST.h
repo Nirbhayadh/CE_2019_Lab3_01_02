28bc36d2f537b25733b870fa043a337e75a72735
Aayam Adhikari <thisisleo7@gmail.com>
2021-07-19 13:01:20 +0545

    Added iterative remove and recurssive inorder traversal

--- include/LinkedBST.h
+++ include/LinkedBST.h
@@ -10,10 +10,18 @@ class LinkedNode
     LinkedNode *left;
     LinkedNode *right;
 
-    LinkedNode(int key, int value, LinkedNode *left, LinkedNode *right)
+    // default constructor
+    LinkedNode()
+    {
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+
+    // parametrized constructor
+    LinkedNode(int key, int data, LinkedNode *left, LinkedNode *right)
     {
         this->key = key;
-        this->data = value;
+        this->data = data;
         this->left = left;
         this->right = right;
     }
@@ -24,18 +32,21 @@ class LinkedBST:public AbstractBST
 {
     private:
     LinkedNode *root;
-    void add(LinkedNode *subtree_Root, LinkedNode *newNode);
+    void add(LinkedNode *subtree_Root, LinkedNode *newNode); // for recurssive add
+    void inorder(LinkedNode *node, char separator = ' '); // for recurssive inorder traversal
+    
+    // void remove(LinkedNode *subtree_Root, int delete_key); // for recurssive delete
 
     public:
     LinkedBST(); // constructor
-    // ~LinkedBST(); // destructor
+    ~LinkedBST(); // destructor
 
     virtual bool isEmpty();
     virtual bool add(int key, int value);
     virtual void max(int &output);
     virtual void min(int &output);
     virtual bool exists(int targetkey);
+    virtual bool remove(int delete_key);
 
-    // virtual void inorder() = 0;
-
+    virtual void inorder();
 };
--- src/LinkedBST.cpp
+++ src/LinkedBST.cpp
@@ -5,11 +5,17 @@ LinkedBST::LinkedBST()
     root = nullptr;
 }
 
+LinkedBST::~LinkedBST()
+{
+    delete root;
+}
+
 bool LinkedBST::isEmpty()
 {
     return root == nullptr;
 }
 
+// Recurssive add node
 bool LinkedBST::add(int key,int value)
 {
     if(exists(key))
@@ -59,6 +65,7 @@ void LinkedBST::add(LinkedNode *subtree_Root, LinkedNode *newNode)
     }
 }
 
+// To find the node with the maximum key in the tree
 void LinkedBST::max(int &output)
 {
     if(!isEmpty())
@@ -76,6 +83,7 @@ void LinkedBST::max(int &output)
     }
 }
 
+// To find the node with the minimum key in the tree
 void LinkedBST::min(int &output)
 {
     if(!isEmpty())
@@ -93,6 +101,7 @@ void LinkedBST::min(int &output)
     }
 }
 
+// To search for a particular node with a given key
 bool LinkedBST::exists(int targetkey)
 {
     if(this->isEmpty())
@@ -120,4 +129,125 @@ bool LinkedBST::exists(int targetkey)
         return false;
     }
     return true;
+}
+
+// Recurssive Inorder Traversal
+void LinkedBST::inorder() {
+    cout << "Inorder traversal of the BST is: " << endl;
+    this->inorder(this->root);
+    cout << endl;
+}
+
+void LinkedBST::inorder(LinkedNode *root_node, char separator) {
+    if (root_node != nullptr) {
+        inorder(root_node->left);
+        cout << root_node->key << separator;
+        inorder(root_node->right);
+    } 
+}
+
+// Iterative Delete Node
+bool LinkedBST::remove(int delete_key)
+{
+    bool deleted = false;
+    LinkedNode *previous = nullptr;
+    LinkedNode *current = this->root;
+
+    while(current != nullptr && current->key != delete_key)
+    {
+        previous = current;
+        if(delete_key > current->key)
+        {
+            current = current->right;
+        }
+        else
+        {
+            current = current->left;
+        }
+    }
+
+    if(current == nullptr)
+    {
+        cout << "Key = " << delete_key << " not found in the BST" << endl;
+        return deleted;
+    }
+
+    if(current->left == nullptr && current->right == nullptr)
+    {
+        if(previous == nullptr)
+        {
+            delete current;
+            deleted = true;
+        }
+        if(current == previous->left)
+        {
+            previous->left = nullptr;
+            delete current;
+            deleted = true;
+        }
+        else
+        {
+            previous->right = nullptr;
+            delete current;
+            deleted = true;
+        }
+    }
+    else if(current->left == nullptr || current->right == nullptr)
+    {
+        LinkedNode *newCurrent;
+        if(current->left == nullptr)
+        {
+            newCurrent = current->right;
+        }
+        else
+        {
+            newCurrent = current->left;
+        }
+
+        // if node to be deleted (current) is the root
+        if(previous == nullptr) 
+        {
+            delete current;
+            deleted = true;
+        }
+
+        if(current == previous->left)
+        {
+            previous->left = newCurrent;
+        }
+        else
+        {
+            previous->right = newCurrent;
+        }
+        
+        delete current;
+        deleted = true;
+    }
+
+    // node to be deleted has two children
+    else
+    {
+        LinkedNode *p = nullptr;
+        LinkedNode *temp;
+
+        temp = current->right;
+        while(temp->left != nullptr)
+        {
+            p = temp;
+            temp = temp->left;
+        }
+
+        if(p != nullptr)
+        {
+            p->left = temp->right;
+        }
+        else
+        {
+            current->right = temp->right;
+        }
+        current->key = temp->key;
+        delete temp;
+        deleted = true;
+    }
+    return deleted;
 }
